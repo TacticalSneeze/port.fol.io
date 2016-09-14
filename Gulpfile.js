eaa4a0bccac6e518e8d80e937f863edf92745d92
Jonathon <Jonathon@Jonathons-MBP.verizon.net>
2016-09-14 16:51:15 -0400

    code cleanup

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,87 +1,46 @@
-var gulp 					= require('gulp'),
-	autoprefixer 			= require('gulp-autoprefixer'),
-	cache 					= require('gulp-cache'),
-	clean 					= require('gulp-clean'),
-	concat 					= require('gulp-concat'),
-	minifyCSS 				= require('gulp-minify-css'),
-	plumber 				= require('gulp-plumber'),
-	rename 					= require('gulp-rename'),
-	sass 					= require('gulp-ruby-sass'),
-	sequence 				= require('run-sequence'),
-	uglify 					= require('gulp-uglify'),
+"use strict";
 
-	/**
-	 * If developing this package set to true but make sure you change it to false before
-	 * deploying and re-run gulp
-	 */
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var bourbon = require("node-bourbon").includePaths;
+var neat = require("node-neat").includePaths;
+var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
 
-	local					= true,
-	destination 			= 'assets/';
-
-
-/**
- * setup stream for package's sass files
- * @return gulp object
- */
-
-gulp.task( 'styles', function()
-{
-	var _gulp = gulp.src( 'scss/**/*.scss' )
-	.pipe( plumber() )
-	.pipe( sass() )
-	.pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) );
-	// don't bother with source map rubbish...
-	if( ! local ) _gulp.pipe( minifyCSS() );
-	_gulp.pipe( gulp.dest( destination + 'css' ) );
-	return _gulp;
+// Compile SASS into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    gulp.src('src/scss/**/*.scss')
+      .pipe(sass(
+      {
+        includePaths: bourbon,
+        includePaths: neat,
+        errLogToConsole: true
+      }))
+      .pipe(gulp.dest('src/css'))
+      .pipe(browserSync.stream());
 });
 
-/**
- * setup stream for package's javascript files
- * @return gulp object
- */
+// PostCSS Task(s)
+var plugins = [
+    require('autoprefixer')({ browsers: ['last 2 versions', 'ie 6-8', 'Firefox > 20']})
+];
 
-gulp.task( 'scripts', function()
-{
-	var _gulp = gulp.src([
-		'js/fsvs.js'
-	])
-	.pipe( plumber() )
-	.pipe( concat( 'bundle.js' ) );
-	if( ! local ) _gulp.pipe( uglify() );
-	_gulp.pipe( gulp.dest( destination + 'js' ) );
-	return _gulp;
+gulp.task('style', function () {
+    return gulp.src('src/css/style.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('dist/css/'))
 });
 
-/**
- * [description]
- * @return {[type]} [description]
- */
-
-gulp.task( 'clean', function()
-{
-	return gulp.src([
-		destination + 'css',
-		destination + 'js'
-	], { read: false } )
-	.pipe( clean({ force : true }) );
+// Static Server + Watch SCSS/HTML files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-/**
- * [description]
- * @return {[type]} [description]
- */
+gulp.task('default', ['serve']);
 
-gulp.task( 'watch', function () {
-	gulp.watch( 'scss/**/*.scss', ['styles']);
-	gulp.watch( 'js/*.js', ['scripts'] );
-});
 
-/**
- * [description]
- * @return {[type]} [description]
- */
 
-gulp.task( 'default', ['clean'], function() {
-        sequence( 'styles', 'scripts' );
-});
